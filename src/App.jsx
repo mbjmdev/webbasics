6f5b181d1c6f348478b89665d98805c84c44a9bb
mbjmdev <151159134+mbjmdev@users.noreply.github.com>
2025-03-20 12:58:14 +0530

    array manipulation

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,37 @@ import Cars from "./components/Cars";
 function App() {
   const originalCars = [
     {
+      index: 1,
       name: "Honda",
       year: 2020,
     },
     {
+      index: 2,
       name: "Honda",
       year: 2000,
     },
     {
+      index: 3,
       name: "Honda",
       year: 1999,
     },
     {
+      index: 4,
       name: "Hyundai",
       year: 2021,
     },
     {
+      index: 5,
       name: "Suzuki",
       year: 2022,
     },
     {
+      index: 6,
       name: "Benz",
       year: 2023,
     },
     {
+      index: 7,
       name: "Audi",
       year: 2020,
     },
@@ -37,20 +44,59 @@ function App() {
   const [search, setSearch] = useState("");
   const [name, setName] = useState("");
   const [year, setYear] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editName, setEditName] = useState("");
+  const [editYear, setEditYear] = useState("");
+  const [editIndex, setEditIndex] = useState("");
   const searchCar = () => {
     let newcar = cars.filter((c) => c.name == search);
     setCars(newcar);
   };
   const addCar = () => {
-    const newcar = {
-      name: name,
-      year: year,
-    };
-    setCars((originalCars) => [...originalCars, newcar]);
+    const newcar = [
+      ...cars,
+      {
+        index: cars.length + 1,
+        name: name,
+        year: year,
+      },
+    ];
+    setCars(newcar);
     setName(" ");
     setYear(" ");
   };
-  const showCars = () => setCars(originalCars);
+
+  const deleteRandom = () => {
+    const newcars = [...cars];
+    newcars.pop();
+    setCars(newcars);
+  };
+  const showCars = () => setCars(cars);
+  const deleteCar = (index) => {
+    const updatedCars = cars.filter((c) => c.index !== index);
+    setCars(updatedCars);
+  };
+
+  const editCar = ({ index, name, year }) => {
+    setEditName(name);
+    setEditYear(year);
+    setEditIndex(index);
+    setEdit(true);
+  };
+
+  const updateCar = () => {
+    cars[editIndex] = {
+      name: editName,
+      year: editYear,
+    };
+    const newcars = [...cars];
+    setCars(newcars);
+    setEditName("");
+    setEditYear("");
+    setEditIndex("");
+    setEdit(false);
+  };
+
   return (
     <>
       <div>
@@ -62,6 +108,7 @@ function App() {
         />
         <button onClick={searchCar}>Search</button>
         <button onClick={showCars}>Show All Cars</button>
+        <button onClick={deleteRandom}>Delete Last Car</button>
       </div>
       <div>
         <input
@@ -76,8 +123,32 @@ function App() {
         />
         <button onClick={addCar}>Add New Car</button>
       </div>
-      {cars.map((c) => (
-        <Cars name={c.name} year={c.year} />
+      {edit ? (
+        <div style={{ marginTop: "15px", marginBottom: "15px" }}>
+          <input
+            type="text"
+            onChange={(e) => setEditName(e.target.value)}
+            value={editName}
+          />
+          <input
+            type="text"
+            onChange={(e) => setEditYear(e.target.value)}
+            value={editYear}
+          />
+          <button onClick={updateCar}>Edit Car</button>
+        </div>
+      ) : (
+        ""
+      )}
+      {cars.map((c, index) => (
+        <Cars
+          name={c.name}
+          year={c.year}
+          key={index}
+          index={index}
+          onDelete={deleteCar}
+          onEdit={editCar}
+        />
       ))}
     </>
   );
--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -9,11 +9,20 @@ const divStyle = {
   display: "inline-block",
 };
 
+/*
+props = {index:"", name:"", year: "", onDelete: "", onEdit: ""}
+Destructuring
+function Cars({index, name, year, onDelete, onEdit})
+*/
+
 export default function Cars(props) {
   return (
     <div style={divStyle}>
+      <h3 className="text">Key: {props.index}</h3>
       <h3 className="text">Car Name: {props.name}</h3>
       <h3 className="text">Year: {props.year}</h3>
+      <button onClick={() => props.onDelete(props.index)}>Delete</button>
+      <button onClick={() => props.onEdit(props)}>Edit</button>
     </div>
   );
 }
